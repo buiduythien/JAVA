93524ecee748baa8b5d9a1fc5a2a5f8786a44a10
buiduythien <73396241+buiduythien@users.noreply.github.com>
2020-12-22 20:44:53 +0300

    Update HelperClass.java

--- src/ua/khpi/oop/lab4/HelperClass.java
+++ src/ua/khpi/oop/lab4/HelperClass.java
@@ -21,9 +21,11 @@ public class HelperClass {
     {
         System.out.println("Aвтор : Буй Зуі КІТ-119а.");
         System.out.println("Ввести текст : У тексті слова заданої довжини замінити зазначеним рядком. Вивести початковий текст та результат.");
-        System.out.println("Пунктів меню : Enter the data, Perfom a task, End program.");
-        System.out.println("Параметрів командного рядка : getWords.");
-        System.out.println();
+        System.out.println("\nMENU:\n1) Input text            --- allows you to enter text");
+        System.out.println("2) Task                  --- performs the task with your text, that you input in 1 action");
+        System.out.println("3) Show input text       --- output your text, that you input in 1 action");
+        System.out.println("4) Show result           --- shows the result of the task");
+        System.out.println("5) Exit program          --- ends the program");
     }
 
     public void outputText(StringBuilder text) {
